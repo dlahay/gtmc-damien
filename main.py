82187bf953bf8763bfe7fa3cf6429fb703fd5a08
salamenthe <91618610+salamenthe@users.noreply.github.com>
2022-09-30 11:34:16 +0200

    Implementation of stage by stage viewing

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import glob
+import os
 from dash import Dash, dcc, Output, Input # pip install dash
 import dash_bootstrap_components as dbc   # pip install dash-bootstrap-components
 import plotly.express as px
@@ -16,8 +18,13 @@ dtypes = {'Jour':'float','Distance':'float','Denivele':'float','Duree':'float','
 parse_dates = ['Duree', 'DureeMoy']
 df = pd.read_csv("2022_GTMC_Damien_Statistiques.csv", usecols=columns, dtype=dtypes, parse_dates=parse_dates)
 
-
-
+parsedGPXPath = 'ParsedGPX'
+os.chdir(parsedGPXPath)
+listEtape = glob.glob('*.csv')
+print(listEtape)
+listEtape = [etape.split('.')[:-1][0] for etape in listEtape]
+print(listEtape)
+os.chdir('..')
 
 # Define the graphs
 fig_denivele_vmoy = px.scatter(df, x='Denivele', y='VMoy', color='Jour', hover_name='Jour', size='Distance')
@@ -26,28 +33,7 @@ fig_dist_vmoy = px.scatter(df, x='Distance', y='VMoy', color='Jour', hover_name=
 fig_distance = px.bar(data_frame=df, x="Jour", y='Distance', orientation='v')
 fig_denivele = px.bar(data_frame=df, x="Jour", y='Denivele', orientation='v')
 fig_duree = px.bar(data_frame=df, x="Jour", y='Duree', orientation='v')
-'''fig_etape = px.scatter_mapbox(
-    etape, 
-    lat="latitude", 
-    lon="longitude",
-    hover_name="time",
-    hover_data={
-        "latitude": ":.2f",
-        "longitude": ":.2f",
-        "altitude": ":.2f m."
-    },
-    color="altitude",
-    zoom=11, 
-    height=500,
-)
 
-fig_etape.update_layout(
-    margin=dict(r=0, t=0, l=0, b=0),
-    mapbox_style="open-street-map",
-    )
-
-fig_etape.update_traces(hovertemplate='<b>%{hovertext}</b><br><br>(%{customdata[0]:.2f}, %{customdata[1]:.2f})<br>Elev. %{customdata[2]:} .ft<extra></extra>')
-'''
 
 # Build your components
 app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
@@ -59,7 +45,7 @@ graph_dist_vmoy = dcc.Graph(figure=fig_dist_vmoy)
 graph_dist = dcc.Graph(figure=fig_distance)
 graph_denivele = dcc.Graph(figure=fig_denivele)
 graph_duree = dcc.Graph(figure=fig_duree)
-#graph_etape = dcc.Graph(figure=fig_etape)
+graph_etape = dcc.Graph(figure={})
 
 
 
@@ -69,7 +55,7 @@ dropdown = dcc.Dropdown(options=df.columns.values[1:],
                         value='Distance',  # initial value displayed when page first loads
                         clearable=False)
 
-dropdown_etape = dcc.Dropdown(options=[],value='GTMC Complète',clearable=False)
+dropdown_etape = dcc.Dropdown(options=listEtape,value='GTMC Complète',clearable=False)
 
 # Customize your own Layout
 # ajouter les widgets sur le layout
@@ -93,7 +79,13 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([dropdown], width=6)
     ], justify='center'),
-    #dbc.Row([graph_etape])
+    dbc.Row([
+        dbc.Col([graph_etape])
+    ],justify='center'),
+    dbc.Row([
+        dbc.Col([dropdown_etape], width=6)
+    ], justify='center'),
+
 
 ], fluid=True)
 
@@ -110,8 +102,47 @@ def update_graph(column_name):  # function arguments come from the component pro
     fig = px.bar(data_frame=df, x="Jour", y=column_name, orientation='v')
     return fig, "# "+column_name 
 
+@app.callback(
+    Output(graph_etape, component_property='figure'),
+    Input(dropdown_etape, component_property='value')
+)
+def update_etape(etapeId):
+    # TODO! 
+    #Open the corresponding CSV
+    os.chdir(parsedGPXPath)
+    etape = pd.read_csv(etapeId+".csv")
+    os.chdir('..')
+
+    #generate the mapbox figure
+    fig_etape = px.scatter_mapbox(
+        etape, 
+        lat="latitude", 
+        lon="longitude",
+        hover_name="time",
+        hover_data={
+            "latitude": ":.2f",
+            "longitude": ":.2f",
+            "altitude": ":.2f m."
+        },
+        color="altitude",
+        zoom=11, 
+        height=500,
+    )
+
+    fig_etape.update_layout(
+        margin=dict(r=0, t=0, l=0, b=0),
+
+        mapbox_style="open-street-map",
+        )
+
+    fig_etape.update_traces(hovertemplate='<b>%{hovertext}</b><br><br>(%{customdata[0]:.2f}, %{customdata[1]:.2f})<br>Elev. %{customdata[2]:} .ft<extra></extra>')
+
+    return fig_etape
+
+
 
 # Run app
+
 if __name__=='__main__':
     app.run_server(debug=True)
     
